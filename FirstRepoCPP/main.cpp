70792a91574e9a14d633d818fe03ecc4f9022951
fr0lin <zaverdeno@gmail.com>
2024-06-07 17:33:28 +0300

    Update main.cpp

--- FirstRepoCPP/main.cpp
+++ FirstRepoCPP/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+int print(int* arr, int size) {
+	for(int i = 0; i < size; i++) 
+		{
+			std::cout << arr[i] << ' ';
+		}
+}
+
 int main() {
 	std::cout << "Hello World";
 }
